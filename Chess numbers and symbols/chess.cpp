2485b5d0fe1caa563fb25b6948293e9dce4435c2
Pav3l4istyakov <pavel.chistyakov.06@gmail.com>
2024-04-29 19:07:17 +0500

    Добавьте файлы проекта.

---
+++ Chess numbers and symbols/chess.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+int main()
+{
+	int size = 1;
+
+	for (int i = 0; i < size; i++) {
+			
+				std::cout << "8,7,6,5,4,3,2,1" << "\n";
+			
+				std::cout << "a,a8,a7,a6,a5,a4,a3,a2,a" << "\n";
+			
+				std::cout << "b8,b7,b6,b5,b4,b3,b2,b"<<"\n";
+				std::cout << "c,c8,c7,c6,c5,c4,c3,c2,c" << "\n";
+				std::cout << "d,d8,d7,d6,d5,d4,d3,d2,d" << "\n";
+				std::cout << "e,e8,e7,e6,e5,e4,e3,e2,e" << "\n";
+				std::cout << "e,e8,e7,e6,e5,e4,e3,e2,e" << "\n";
+				std::cout << "f,f8,f7,f6,f5,f4,f3,f2,f" << "\n";
+				std::cout << "g,g8,g7,g6,g5,g4,g3,g2,g" << "\n";
+				std::cout << "h,h8,h7,h6,h5,h4,h3,h2,g" << "\n";
+				std::cout << "8,7,6,5,4,3,2,1" << "\n";
+			
+
+
+
+
+
+		}
+		return 0;
+	}
